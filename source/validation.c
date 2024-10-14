e0d2feb8c1d622839f4ecbaf5b11b0604e4fb85d
Adeline <adelineeminian13@gmail.com>
2024-10-15 00:19:53 +0400

    map validating

--- source/validation.c
+++ source/validation.c
@@ -31,7 +31,7 @@ int	validating_texture(char *line, t_cub3d *cub)
 		return (0);
 	}
 	else if (cub->texture_flag <= 4)
-		error("Wrong input");
+		error("Wrong input for textures");
 	return (1);
 }
 
@@ -40,7 +40,7 @@ int	validating_color(char *line, t_cub3d *cub)
 	char		**args;
 	t_color		F;
 	t_color		C;
-
+	
 	args = ft_split(line, ',');
 	if (!ft_strcmp("F", args[0]) && ++cub->texture_flag)
 	{
@@ -50,25 +50,37 @@ int	validating_color(char *line, t_cub3d *cub)
 		F.blue = (atoi(args[3]));
 		if (F.blue < 0 || F.blue > 255 || F.red < 0 \
 			|| F.red > 255	|| F.green < 0 || F.green > 255)
-			error("the color should be in 0-255 range");
+			error("the floor color should be in 0-255 range");
 		cub->F_color = rgb(F.red, F.green, F.blue);
 	}
-	else if (!ft_strcmp("C", args[0]) && ++cub->texture_flag)
+	else if (!ft_strcmp("C", args[0]) && cub->texture_flag++)
 	{
 		C.red = (atoi(args[1]));
 		C.green = (atoi(args[2]));
 		C.blue = (atoi(args[3]));
 		if (C.blue < 0 || C.blue > 255 || C.red < 0 \
 			|| C.red > 255	|| C.green < 0 || C.green > 255)
-			error("the color should be in 0-255 range");
+			error("the cell color should be in 0-255 range");
 		cub->C_color = rgb(C.red, C.green, C.blue);
 	}
-	else if (cub->texture_flag > 6 && cub->texture_flag >= 4 && is_space(*line))
-		error("Wrong input");
+	else if (cub->texture_flag < 6 && cub->texture_flag >= 4)
+		error("There is not any cell or floor color");
 	return (1);
 }
 
-int	parsing(char *filename, t_cub3d *cub)
+void	parsing_map(char *line, t_cub3d *cub)
+{
+	while (++(*line))
+	{
+		if (*line != '1' && *line != '0' && *line != '\0' && *line != '\n')
+	{printf("%c\n", *line);
+			error("The map should only contain 0 and 1");
+	}}
+	(void)cub;
+
+}
+
+void	parsing(char *filename, t_cub3d *cub)
 {
 	int		fd;
 	char	*line;
@@ -77,27 +89,24 @@ int	parsing(char *filename, t_cub3d *cub)
 	if (fd > 0)
 	{
 		while (1)
-		{	
-			while (cub->texture_flag <= 6)
+		{
+			line = get_next_line(fd);
+			if (!line)
+				return ;
+			line = trim(line);
+			if (*line != 0)
 			{
-				line = get_next_line(fd);
-				if (!line)
-					return (0);
-				line = trim(line);
-				if (*line != 10)
-				{
-					// printf("")
-					if (cub->texture_flag < 4 && !validating_texture(line, cub))
-						continue ;
-					if (cub->texture_flag < 6 && cub->texture_flag >= 4 && !validating_color(line, cub))
-						continue ;
-
-				}
+				if (cub->texture_flag == 6)
+					parsing_map(line, cub);
+				if (cub->texture_flag < 4 && !validating_texture(line, cub))
+					continue ;
+				if (cub->texture_flag < 6 && cub->texture_flag >= 4 \
+					&& !validating_color(line, cub))
+					continue ;
 			}
 		}
 	}
 	error("file doesn't exist.");
-	return(1);
 }
 
 int	check_filename(char *filename)
@@ -116,17 +125,14 @@ int	check_filename(char *filename)
 	{
 		if (ft_strcmp(ft_substr(filename, len - 3, 4), ".cub") == 0)
 		{
-			if (!parsing(filename, cub))
-			{
+			parsing(filename, cub);
 				printf("%s-------------%s\n", "WE", cub->texture->west);
 				printf("%s-------------%s\n", "SO", cub->texture->south);
 				printf("%s-------------%s\n", "EA", cub->texture->east);
 				printf("%s-------------%s\n", "no", cub->texture->north);
 				printf("%s-------------%d\n", "c", cub->C_color);
 				printf("%s-------------%d\n", "WE", cub->F_color);
-
-				return (0);
-			}
+				return 0;
 		}
 	}
 	error("passed file should be with <.cub>");
